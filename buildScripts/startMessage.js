73b4c33aca0605ba453bbbb0682e6382ba738679
Tuan Anh Le <anhlt1983@gmail.com>
2017-10-21 14:07:42 +0900

    using new synx tax of ESS6

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,4 @@
-var chalk = require("chalk");
+//var chalk = require("chalk");
+import chalk from 'chalk';
 
-console.log(chalk.blue('Starting app in dev mode...'));
+console.log(chalk.green('Starting app in dev mode...'));
